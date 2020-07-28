4957ca70724cd14910de56255fc61cbd451a386e
DilanRamirez <drramirezra@miners.utep.edu>
2020-07-28 14:42:52 -0700

    Updates

---
+++ precache-manifest.aa325507b18b6d6babc0adb1fcb95511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e53f7dc936b43a7a44d810b78b58b8",
+    "url": "/trivia-app/index.html"
+  },
+  {
+    "revision": "fd9d85a4be34bbe47a59",
+    "url": "/trivia-app/static/css/main.8fa60e27.chunk.css"
+  },
+  {
+    "revision": "f30a872f933aee927635",
+    "url": "/trivia-app/static/js/2.8113c42d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trivia-app/static/js/2.8113c42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9d85a4be34bbe47a59",
+    "url": "/trivia-app/static/js/main.a999fb7c.chunk.js"
+  },
+  {
+    "revision": "15ec67f38d36a05e8619",
+    "url": "/trivia-app/static/js/runtime-main.51f2d099.js"
+  }
+]);
